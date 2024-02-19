254f365d0036b8e13bae77b0fb4b6068744e0501
Nicole Farinello <nicole_mattos@live.com>
2024-02-19 15:13:08 +0000

    comitting stupid coaching exercise in rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question]
+    if params[:question] == "I am going to work"
+      @response = "Great"
+    elsif params[:question].ends_with?("?")
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = " I don't care, get dressed and go to work!"
+    end
+  end
+end
